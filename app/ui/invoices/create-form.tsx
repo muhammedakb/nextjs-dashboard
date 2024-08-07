45ef551d7a93928a82b6f1bd44974a3b88e63f97
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-08-07 16:17:09 +0300

    feat: mutating data (update & delete)

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,24 +7,34 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+    /* 
+     INFO: 
+     HTML'de, action özelliğine bir URL iletiriz.
+     Bu URL, form verilerimizin gönderilmesi gereken hedef olacaktır (genellikle bir API endpoint'i).
+     React'ta action özelliği özel bir prop olarak kabul edilir. 
+     Yani react, action'ların çağrılmasına izin vermek için bunun üzerine inşa edilir.
+     Perde arkasında server action'lar bir POST API endpoint'i oluşturur.
+     Bu nedenle server action'ları kullanırken API endpoint'lerini manuel olarak oluşturmamıza gerek yoktur.
+     */
+    <form action={createInvoice}>
+      <div className='rounded-md bg-gray-50 p-4 md:p-6'>
         {/* Customer Name */}
-        <div className="mb-4">
-          <label htmlFor="customer" className="mb-2 block text-sm font-medium">
+        <div className='mb-4'>
+          <label htmlFor='customer' className='mb-2 block text-sm font-medium'>
             Choose customer
           </label>
-          <div className="relative">
+          <div className='relative'>
             <select
-              id="customer"
-              name="customerId"
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
+              id='customer'
+              name='customerId'
+              className='peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
+              defaultValue=''
             >
-              <option value="" disabled>
+              <option value='' disabled>
                 Select a customer
               </option>
               {customers.map((customer) => (
@@ -33,79 +43,79 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </option>
               ))}
             </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <UserCircleIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500' />
           </div>
         </div>
 
         {/* Invoice Amount */}
-        <div className="mb-4">
-          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+        <div className='mb-4'>
+          <label htmlFor='amount' className='mb-2 block text-sm font-medium'>
             Choose an amount
           </label>
-          <div className="relative mt-2 rounded-md">
-            <div className="relative">
+          <div className='relative mt-2 rounded-md'>
+            <div className='relative'>
               <input
-                id="amount"
-                name="amount"
-                type="number"
-                step="0.01"
-                placeholder="Enter USD amount"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id='amount'
+                name='amount'
+                type='number'
+                step='0.01'
+                placeholder='Enter USD amount'
+                className='peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500'
               />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <CurrencyDollarIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
             </div>
           </div>
         </div>
 
         {/* Invoice Status */}
         <fieldset>
-          <legend className="mb-2 block text-sm font-medium">
+          <legend className='mb-2 block text-sm font-medium'>
             Set the invoice status
           </legend>
-          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
-              <div className="flex items-center">
+          <div className='rounded-md border border-gray-200 bg-white px-[14px] py-3'>
+            <div className='flex gap-4'>
+              <div className='flex items-center'>
                 <input
-                  id="pending"
-                  name="status"
-                  type="radio"
-                  value="pending"
-                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  id='pending'
+                  name='status'
+                  type='radio'
+                  value='pending'
+                  className='h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2'
                 />
                 <label
-                  htmlFor="pending"
-                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
+                  htmlFor='pending'
+                  className='ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600'
                 >
-                  Pending <ClockIcon className="h-4 w-4" />
+                  Pending <ClockIcon className='h-4 w-4' />
                 </label>
               </div>
-              <div className="flex items-center">
+              <div className='flex items-center'>
                 <input
-                  id="paid"
-                  name="status"
-                  type="radio"
-                  value="paid"
-                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  id='paid'
+                  name='status'
+                  type='radio'
+                  value='paid'
+                  className='h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2'
                 />
                 <label
-                  htmlFor="paid"
-                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
+                  htmlFor='paid'
+                  className='ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white'
                 >
-                  Paid <CheckIcon className="h-4 w-4" />
+                  Paid <CheckIcon className='h-4 w-4' />
                 </label>
               </div>
             </div>
           </div>
         </fieldset>
       </div>
-      <div className="mt-6 flex justify-end gap-4">
+      <div className='mt-6 flex justify-end gap-4'>
         <Link
-          href="/dashboard/invoices"
-          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+          href='/dashboard/invoices'
+          className='flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200'
         >
           Cancel
         </Link>
-        <Button type="submit">Create Invoice</Button>
+        <Button type='submit'>Create Invoice</Button>
       </div>
     </form>
   );
